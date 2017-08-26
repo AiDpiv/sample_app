ac210756971a26a5dcae51b5b1d82682788bc554
AiDpiv <aidpiv@gmail.com>
2017-08-26 09:29:55 +0300

    session l1

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email.downcase])
+    if user && user.authenticate(params[:session][:password])
+  else
+    # error message
+    render 'new'
+  end
+
+
+
+  def destroy
+
+  end
+end
