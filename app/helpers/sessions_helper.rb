18866a41742fb137e9da6100f07a3392d44727ef
AiDpiv <aidpiv@gmail.com>
2017-09-02 09:51:28 +0300

    Finish login without remember

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,19 @@
 module SessionsHelper
+  #осуществляет вход пользователя
   def log_in(user)
     session[:user_id]=user.id
+  end
+  # Возвращает текущего вошедшего пользователя (если есть)
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  # Return true, if user is loged id, else returns false
+  def logged_in?
+    !current_user.nil?
+  end
+  # Осуществляет выход текущего пользователя.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,18 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+
+  # Возвращает дайджест для указанной строки.
+    def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ?
+             BCrypt::Engine::MIN_COST :
+             BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+    # Возвращает случайный токен.
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
+
 end
